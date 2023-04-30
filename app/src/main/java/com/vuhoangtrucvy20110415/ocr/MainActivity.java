0c98046f19d1aa8b8565ec99b35bdb3f69e1a742
Nem <canhnamtran216@gmail.com>
2023-04-30 09:52:01 +0700

    MLKit Integrated

--- app/src/main/java/com/vuhoangtrucvy20110415/ocr/MainActivity.java
+++ app/src/main/java/com/vuhoangtrucvy20110415/ocr/MainActivity.java
@@ -1,55 +1,86 @@
 package com.vuhoangtrucvy20110415.ocr;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.content.ContentValues;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-import android.Manifest;
 
-import com.google.android.gms.vision.text.TextRecognizer;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
 import com.vuhoangtrucvy20110415.ocr.databinding.ActivityMainBinding;
 
-import org.w3c.dom.Text;
+import java.io.FileNotFoundException;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final int STORAGE_REQUEST_CODE=400;
-    private static final int IMAGE_PICK_GALLERY_CODE=1000;
+//    private static final int IMAGE_PICK_GALLERY_CODE=1000;
+    ActivityMainBinding binding;
     EditText mResult;
     ImageView mPreview;
-    String storagePermission[];
+    Bitmap bitmap;
+    String[] storagePermission;
     Uri image_uri;
+    private GraphicOverlay mGraphicOverlay;
+
+    final ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+        new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri uri) {
+                image_uri=uri;
+                try {
+                    bitmap= BitmapFactory.decodeStream(getContentResolver().openInputStream(image_uri));
+                    mPreview.setImageBitmap(bitmap);
+                    if (bitmap!=null){
+                        Log.e("bitmap",bitmap.toString());
+                        runTextRecognition();
+                    }
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ActionBar actionBar=getSupportActionBar();
+        assert actionBar != null;
         actionBar.setSubtitle("Click + button to insert Image");
-        mPreview=findViewById(R.id.imageView);
+        mPreview = binding.imageView;
+        mGraphicOverlay = binding.graphicOverlay;
+        mResult = binding.result;
+        mResult.setText("");
         storagePermission=new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
     }
 
     @Override
@@ -77,10 +108,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void pickGallery() {
-        Intent intent=new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), IMAGE_PICK_GALLERY_CODE);
+        mGetContent.launch("image/*");
     }
 
     private void requestStoragePermission() {
@@ -88,46 +116,122 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean checkStoragePermission() {
-        boolean result=ContextCompat.checkSelfPermission(this,
+        return ContextCompat.checkSelfPermission(this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
-        return result;
+
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case STORAGE_REQUEST_CODE:
-                if (grantResults.length>0){
-                    boolean writeStorageAccepted=grantResults[0]==
-                            PackageManager.PERMISSION_GRANTED;
-                    if (writeStorageAccepted){
-                        pickGallery();
-                    }
-                    else{
-                        Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
-                    }
+        if (requestCode == STORAGE_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+                boolean writeStorageAccepted = grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED;
+                if (writeStorageAccepted) {
+                    pickGallery();
+                } else {
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
                 }
+            }
         }
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK){
-            if(requestCode==IMAGE_PICK_GALLERY_CODE) {
-                CropImage.activity(data.getData())
-                        .setGuidelines(CropImageView.Guidelines.ON)
-                        .start(this);
-            }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(resultCode==RESULT_OK){
+//            if(requestCode==IMAGE_PICK_GALLERY_CODE) {
+//                assert data != null;
+//                CropImage.activity(data.getData())
+//                        .setGuidelines(CropImageView.Guidelines.ON)
+//                        .start(this);
+//            }
+//        }
+//        if (requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+//            CropImage.ActivityResult result=CropImage.getActivityResult(data);
+//            if (resultCode==RESULT_OK){
+//                assert result != null;
+//                Uri resultUri = result.getUri();
+//                mPreview.setImageURI(resultUri);
+//            }
+//        }
+//    }
+
+    private void runTextRecognition() {
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+        TextRecognizer recognizer = TextRecognition.getClient();
+//        mTextButton.setEnabled(false);
+        // Task failed with an exception
+        //                               mTextButton.setEnabled(true);
+        recognizer.process(image)
+               .addOnSuccessListener(
+                       texts -> {
+//                               mTextButton.setEnabled(true);
+//                           processTextRecognitionResult(texts);
+
+                       })
+               .addOnFailureListener(
+                       Throwable::printStackTrace);
+        recognizer.close();
+    }
+
+    private void showToast(String message, boolean success) {
+
+        Snackbar snackbar = Snackbar.make(binding.getRoot(),message,Snackbar.LENGTH_SHORT);
+
+        if (success) {
+            snackbar.setDuration(Snackbar.LENGTH_LONG);
+            snackbar.setAction("COPY", v -> {
+                copyToClipboard(mResult.getText().toString());
+//                change action button
+                snackbar.setAction("COPIED", v1 -> {
+                    snackbar.setDuration(Snackbar.LENGTH_LONG);
+                    new CountDownTimer(2000, 1000) {
+                       public void onTick(long millisUntilFinished) {
+                       }
+                       public void onFinish() {
+                           snackbar.dismiss();
+                       }
+                    }.start();
+                });
+            });
         }
-        if (requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
-            CropImage.ActivityResult result=CropImage.getActivityResult(data);
-            if (resultCode==RESULT_OK){
-                Uri resultUri = result.getUri();
-                mPreview.setImageURI(resultUri);
-            }
+        snackbar.show();
+    }
+
+    void copyToClipboard(String text) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("extracted", text);
+        clipboard.setPrimaryClip(clip);
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2)
+            Toast.makeText(this , "Copied", Toast.LENGTH_SHORT).show();
+    }
+
+    private void processTextRecognitionResult(Text texts) {
+        List<Text.TextBlock> blocks = texts.getTextBlocks();
+        if (blocks.size() == 0) {
+           showToast("No text found", false);
+           mResult.setText("");
+           return;
         }
+        StringBuilder extracted_text = new StringBuilder();
+        mGraphicOverlay.clear();
+        for (int i = 0; i < blocks.size(); i++) {
+           List<Text.Line> lines = blocks.get(i).getLines();
+           for (int j = 0; j < lines.size(); j++) {
+               List<Text.Element> elements = lines.get(j).getElements();
+               for (int k = 0; k < elements.size(); k++) {
+//                   GraphicOverlay.Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
+//                   mGraphicOverlay.add(textGraphic);
+                   extracted_text.append(elements.get(k).getText()).append(" ");
+               }
+//               Log.e("TAG", "processTextRecognitionResult: "+elements.get(0).getText());
+               extracted_text.append("\n");
+           }
+        }
+        mResult.setText(extracted_text.toString());
+        showToast("Texts are successfully extracted", true);
     }
 }
